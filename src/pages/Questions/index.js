6684c86368df5af845444b370d8c9a20452a9b9d
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-01 09:08:59 -0300

    Styled Questions

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Header, ButtonEdit } from '~/styles/pageTable';
+import { Content } from './styles';
 
 export default function Questions() {
-  return <h1>Questions</h1>;
+  return (
+    <Container>
+      <Header>
+        <h1>Pedidos de auxílio</h1>
+      </Header>
+      <Content>
+        <table>
+          <thead>
+            <tr>
+              <th>ALUNO</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Lennert Nijenbijvank</td>
+              <td>
+                <ButtonEdit type="button">responder</ButtonEdit>
+              </td>
+            </tr>
+            <tr>
+              <td>Lennert Nijenbijvank</td>
+              <td>
+                <ButtonEdit type="button">responder</ButtonEdit>
+              </td>
+            </tr>
+            <tr>
+              <td>Lennert Nijenbijvank</td>
+              <td>
+                <ButtonEdit type="button">responder</ButtonEdit>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/pages/Questions/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 700px;
+  background: #ffffff;
+  border-radius: 4px;
+  padding: 30px 30px 0 30px;
+  margin-top: 20px;
+  font-size: 16px;
+
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    th {
+      text-align: left;
+      font-weight: bold;
+      color: #444444;
+    }
+
+    tbody {
+      tr {
+        td {
+          padding: 16px 0;
+          color: #666666;
+        }
+        & + tr {
+          border-top: 1px solid #eeeeee;
+        }
+      }
+    }
+  }
+`;
